b343cb186cc22377e494c00e6fff1b8f63cecbc0
Marcusu3u <brandon.vegae@gmail.com>
2025-08-14 11:26:06 -0600

    mi primer commit en java

---
+++ IF_2000_ProyectoProgra/src/domain/Person.java
@@ -0,0 +1,78 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package domain;
+
+/**
+ *
+ * @author MSI
+ */
+public class Person {
+    
+    private String name, lastName, id, phone;
+    private int age;
+
+    public Person(String name, String lastName, String id, String phone, int age) {
+        this.name = name;
+        this.lastName = lastName;
+        this.id = id;
+        this.phone = phone;
+        this.age = age;
+    }
+    
+    public Person(){
+        
+        
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    
+    public String toString (){
+        return "Person\n"
+                +"\nname="+getName()
+                +"\nlast name="+getLastName()
+                +"\nid="+getId()
+                +"\nphone="+getPhone()
+                +"\nage="+getAge();
+    }
+    
+}
---
+++ IF_2000_ProyectoProgra/src/if_2000_proyectoprogra/Main.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package if_2000_proyectoprogra;
+
+/**
+ *
+ * @author MSI
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String name = "messi";
+        
+        int age = 12;
+        
+        System.out.println("Hello world"+name+age);
+        
+    }
+    
+}
